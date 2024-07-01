cbded5c4d50ae6159569fc7c4d21e8a0f3e462e7
Hopejrajoku <hajoku0@gmail.com>
2024-07-01 10:32:43 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 //import Button from "./components/Button";
 import Header from "./components/Header";
-import { Hero } from "./components/Hero";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
